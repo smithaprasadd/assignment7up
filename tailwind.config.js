4721921a9c1dbeda8b13233fd27cb98928c35a5a
smithaprasadd <163016594+smithaprasadd@users.noreply.github.com>
2025-12-14 12:26:51 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: "class",  // <--- IMPORTANT
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
